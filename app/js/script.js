dac6727ba2fffd3ef75c5da48f13f8c81d5961a9
Juan Castañeda <51100846+tanecu@users.noreply.github.com>
2022-04-18 14:59:10 -0500

    All Dekstop and Mobile
    Without menu for mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+console.log('Funciona js')
+
+const btnHamburger = document.querySelector('#btnHambur');
+
+btnHamburger.addEventListener('click', function () {
+    console.log('open hamburger');
+    if(btnHamburger.classList.contains('open')){
+        btnHamburger.classList.add('open');
+    }
+    else{
+        btnHamburger.classList.remove('open');
+    }
+});
